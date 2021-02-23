dfac7f7c8c402eaa62a4c15f937124765bf39377
adamjohnlea <imnogeekdptcom@me.com>
2021-02-23 15:59:56 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+<head>
+    <title>First Theme</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset = "<?php bloginfo('charset'); ?>" />
+    <?php wp_head(); ?>
+</head>
+<body>
